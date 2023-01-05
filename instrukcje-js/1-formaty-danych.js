991af821186f34e8877ea4763400f41ed6fdf49e
Robert Kirsz <robert.kirsz@gmail.com>
2023-01-05 11:47:29 +0100

    add javascript files

---
+++ instrukcje-js/1-formaty-danych.js
@@ -0,0 +1,35 @@
+console.log('Hello World')
+
+// Dane liczbowe
+12341.123
+
+// Dane tekstowe
+'abc 123'
+"abc 123"
+`abc 123`
+
+// Dane boolowskie
+true
+false
+
+// Tablice
+[123, 234, 567.12, 'text', false]
+['Working on..', 'Doing ...']
+
+// Obiekt
+{
+  'name': 'Basia',
+  'age': 21,
+  'xxx': 567.12,
+  'id': 123,
+  'xaxc': false,
+  doSomething: () => {}
+}
+
+// Deklaracja funkcja
+addNewTodoItem = (newTodoText) => {
+  document.addDiv(newTodoText)
+}
+
+// Wywołanie funkcji
+addNewTodoItem('Kupić mleko')
---
+++ instrukcje-js/2-zmienne.js
@@ -0,0 +1,22 @@
+console.log('Hello World')
+
+// Stary zapis
+// var score = 0
+// var playerName = 'Basia'
+
+let score = 0
+let playerName = 'Basia'
+const currentDate = '05-01-2023'
+
+console.log('-- Przed modyfikacjami --')
+console.log('Punktacja:', score)
+console.log('Imię gracza:', playerName)
+
+console.log('-- Po modyfikacjach --')
+
+score = 5
+
+console.log('Punktacja:', score)
+
+// To nie zadziała
+// currentDate = '06-01-2023'
