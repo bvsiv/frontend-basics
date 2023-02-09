34c38d84f38f3d3c9842cb0b5a85f710f92b8275
Robert Kirsz <robert.kirsz@gmail.com>
2023-02-09 13:09:00 +0100

    list item status change

--- main.js
+++ main.js
@@ -11,13 +11,20 @@
 // kebab-case
 
 const addTaskButton = document.querySelector('.add-task')
-const upcomingTasksList = document.querySelector('.upcoming-tasks .tasks')
 const deleteButtons = document.querySelectorAll('.deleting-button')
+
+const upcomingTasksList = document.querySelector('.upcoming-tasks .tasks')
+const upcomingTasksListItems = upcomingTasksList.querySelectorAll('div')
+
+const finishedTasksList = document.querySelector('.finished-tasks .tasks')
+const finishedTasksListItems = finishedTasksList.querySelectorAll('div')
 
 const addNewElementToTheList = () => {
   const messageFromUser = window.prompt('Podaj nazwę elementu do listy')
   const listElement = document.createElement('div')
   const listElementText = document.createTextNode(messageFromUser)
+
+  // TODO: Opakuj tekst w span i dodaj przycisk z klasą .deleting-button
 
   listElement.appendChild(listElementText)
   upcomingTasksList.appendChild(listElement)
@@ -29,4 +36,22 @@ deleteButtons.forEach(deleteButton => {
   deleteButton.addEventListener('click', () => {
     deleteButton.parentElement.remove()
   })
+})
+
+upcomingTasksList.addEventListener('click', event => {
+  const clickedItem = event.target
+
+  if (clickedItem.classList.contains('tasks')) return
+  if (clickedItem.classList.contains('deleting-button')) return
+
+  finishedTasksList.append(clickedItem)
+})
+
+finishedTasksList.addEventListener('click', event => {
+  const clickedItem = event.target
+
+  if (clickedItem.classList.contains('tasks')) return
+  if (clickedItem.classList.contains('deleting-button')) return
+
+  upcomingTasksList.append(clickedItem)
 })
