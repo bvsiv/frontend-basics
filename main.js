35ed21b7e7f3f6a974bb2ccbc8491c0863648fb6
Robert Kirsz <robert.kirsz@gmail.com>
2023-03-03 12:16:18 +0100

    save and remove tasks from localStorage

--- main.js
+++ main.js
@@ -10,7 +10,9 @@
 // snake_case
 // kebab-case
 
-const tasksFromLocalStorage = localStorage.getItem('tasks')
+const upcomingTasksFromLocalStorage = localStorage.getItem('upcomingTasks')
+// TODO: implement this
+const finishedTasksFromLocalStorage = localStorage.getItem('finishedTasks')
 
 const addTaskButton = document.querySelector('.add-task')
 
@@ -20,62 +22,74 @@ const upcomingTasksListItems = upcomingTasksList.querySelectorAll('div')
 const finishedTasksList = document.querySelector('.finished-tasks .tasks')
 const finishedTasksListItems = finishedTasksList.querySelectorAll('div')
 
+const addNewElementToTheList = taskText => {
+  if (taskText === '') return
+
+  const listElement = document.createElement('div')
+  const listElementText = document.createTextNode(taskText)
+  const taskNameElement = document.createElement('span')
+  const deleteButton = document.createElement('button')
+
+  listElement.appendChild(listElementText)
+  upcomingTasksList.appendChild(listElement)
+  listElement.appendChild(taskNameElement)
+  listElement.appendChild(deleteButton)
+  taskNameElement.append(listElementText)
+  deleteButton.classList.add('deleting-button')
+
+  addClickListenersToDeleteButtons()
+}
+
+// TODO: reduce indentation
 const addClickListenersToDeleteButtons = () => {
   document.querySelectorAll('.deleting-button').forEach(deleteButton => {
     deleteButton.addEventListener('click', () => {
+      removeElementFromLocalStorage(deleteButton.parentElement.innerText)
       deleteButton.parentElement.remove()
     })
   })
 }
 
+const saveElementInLocalStorage = newTask => {
+  if (newTask === '') return
+
+  const existingTasks = localStorage.getItem('upcomingTasks')
+  const updatedTasks = existingTasks === null ? newTask : existingTasks + ',' + newTask
+
+  localStorage.setItem('upcomingTasks', updatedTasks)
+}
+
+const removeElementFromLocalStorage = taskToRemove => {
+  const existingTasks = localStorage.getItem('upcomingTasks')?.split(',') || []
+  const updatedTasks = existingTasks.filter(taskText => taskText !== taskToRemove)
+
+  localStorage.setItem('upcomingTasks', updatedTasks.join())
+}
+
 /*
 let tasks = []
 
-if (tasksFromLocalStorage !== null) {
-  tasks = tasksFromLocalStorage.split(',')
+if (upcomingTasksFromLocalStorage !== null) {
+  tasks = upcomingTasksFromLocalStorage.split(',')
 }
 
-const tasks = tasksFromLocalStorage ? tasksFromLocalStorage.split(',') : []
+const tasks = upcomingTasksFromLocalStorage ? upcomingTasksFromLocalStorage.split(',') : []
 */
 
-const tasks = tasksFromLocalStorage?.split(',') || []
+const tasks = upcomingTasksFromLocalStorage?.split(',') || []
 
-tasks.forEach(task => {
-  const listElement = document.createElement('div')
-  const listElementText = document.createTextNode(task)
-  const taskName = document.createElement('span')
-  const deleteButton = document.createElement('button')
-
-  listElement.appendChild(listElementText)
-  upcomingTasksList.appendChild(listElement)
-  listElement.appendChild(taskName)
-  listElement.appendChild(deleteButton)
-  taskName.append(listElementText)
-  deleteButton.classList.add('deleting-button')
-
-  addClickListenersToDeleteButtons()
-})
-
-const addNewElementToTheList = () => {
-  const messageFromUser = window.prompt('Podaj nazwę elementu do listy')
-  const listElement = document.createElement('div')
-  const listElementText = document.createTextNode(messageFromUser)
-  const taskName = document.createElement('span')
-  const deleteButton = document.createElement('button')
-
-  listElement.appendChild(listElementText)
-  upcomingTasksList.appendChild(listElement)
-  listElement.appendChild(taskName)
-  listElement.appendChild(deleteButton)
-  taskName.append(listElementText)
-  deleteButton.classList.add('deleting-button')
-
-  addClickListenersToDeleteButtons()
-}
+tasks.forEach(addNewElementToTheList)
 
 addClickListenersToDeleteButtons()
 
-addTaskButton.addEventListener('click', addNewElementToTheList)
+addTaskButton.addEventListener('click', () => {
+  const taskText = window.prompt('Podaj nazwę elementu do listy')
+
+  if (taskText === '') return
+
+  addNewElementToTheList(taskText)
+  saveElementInLocalStorage(taskText)
+})
 
 upcomingTasksList.addEventListener('click', event => {
   const clickedItem = event.target
